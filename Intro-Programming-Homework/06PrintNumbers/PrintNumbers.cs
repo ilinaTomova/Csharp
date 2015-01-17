931521f705672263ab89ac3ef8eeb1bd1903999d
ilinaTomova <ilina.tomova9@gmail.com>
2015-01-17 20:48:15 +0200

    Intro

---
+++ Intro-Programming-Homework/06PrintNumbers/PrintNumbers.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class PrintNumbers
+{
+    static void Main()
+    {
+       int a = 1;
+       int b = 101;
+       int c = 1001;
+       Console.WriteLine("{0}", a);
+       Console.WriteLine("{0}", b);
+       Console.WriteLine("{0}", c);
+      
+    }
+}
+
---
+++ Intro-Programming-Homework/15AgeAfterYears/AgeAfterYears.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+
+
+class AgeAfterYears
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Enter your age:");
+        int Age = int.Parse(Console.ReadLine());
+        Console.WriteLine("Your age after 10 years will be:{0}", Age + 10);
+
+    }
+}
+
